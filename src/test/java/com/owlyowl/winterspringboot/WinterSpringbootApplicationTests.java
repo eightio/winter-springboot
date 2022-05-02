590c0b5567386dfd0ae509daa2c52d4b694878a2
eightio <3003277@qq.com>
2022-05-02 13:53:22 +0800

    🏠 初始化项目

---
+++ src/test/java/com/owlyowl/winterspringboot/WinterSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.owlyowl.winterspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WinterSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
